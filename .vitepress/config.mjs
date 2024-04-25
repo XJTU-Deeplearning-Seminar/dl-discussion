0f0c44f2cd8f52632fea75c42f7256e322097186
Chris Yang <yy4382@outlook.com>
2024-04-25 16:02:55 +0800

    style: lint code

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,29 +3,20 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   themeConfig: {
     siteTitle: "Deep Learning Discussion",
-    nav: [
-      { text: "Home", link: "/" },
-      { text: "研讨会记录", link: "/discussions/" },
-    ],
+    nav: [{ text: "研讨会记录", link: "/discussions/" }],
     sidebar: {
       discussions: [
         {
           text: "第一次",
           collapsed: false,
-          items: [
-            {
-              text: "GNN 简介",link: "/discussions/GNN-intro/"
-            }
-          ],
+          items: [{ text: "GNN 简介", link: "/discussions/GNN-intro/" }],
         },
         {
           text: "第二次",
           collapsed: false,
-          items: [
-            { text: "LLM 初探", link: "/discussions/llm-quick-look/" }],
+          items: [{ text: "LLM 初探", link: "/discussions/llm-quick-look/" }],
         },
       ],
     },
-
   },
 });
